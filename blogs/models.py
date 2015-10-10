9511af96227d58b7d613a5c1ff604c847d3bc002
Benson Mburu <nguruben@gmail.com>
2015-10-10 22:09:33 +0300

    created pagination

--- blogs/models.py
+++ blogs/models.py
@@ -18,4 +18,14 @@ class PostAdmin(admin.ModelAdmin):
     
 admin.site.register(Post,PostAdmin)
 
-def main(request)
+def main(request):
+    posts=Post.objects.all().order_by("-created")
+    paginator=Paginator(posts,2)
+    
+    try:page=int(request.get("page",'1'))
+        except valueError:page=1
+            try:
+                posts=paginator.page(page)
+            except(InvalidPage, EmptyPage):
+                posts=paginator.page(paginator.num_pages)
+            return render_to_response("list.html",dict(posts=posts, user=request.user))
